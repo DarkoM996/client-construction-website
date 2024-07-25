610698db5773ebe6dec1fa38b540be4df584454d
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-25 18:03:13 +0200

    Home page work

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <main className="max-w-screen-3xl mx-auto w-full min-h-screen relative px-6 py-4 lg:px-28">
+      Hero
+    </main>
+  );
 };
 
 export default Hero;
