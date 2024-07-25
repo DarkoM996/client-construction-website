b49f963e8b9ea83e7eee301b398056144dfe4f9b
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-25 18:06:39 +0200

    latest

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import hero from "../../public/images/hero.jpg";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -14,6 +15,37 @@ const Hero = () => {
           // just experiment with different height of the image at different width sizes. The original h-[600px] can also work.
           className="w-full h-[420px] sm:h-[600px] object-cover rounded-3xl bg-center"
         />
+      </div>
+      {/* Text */}
+      <div className="h-full w-full flex flex-col justify-center items-center md:flex-row md:justify-between gap-4 py-5">
+        <div className="w-full md:w-1/2 lg:w-full">
+          <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold text-wrap w-full lg:w-2/3 h-full">
+            Hotel Construction Amplified
+          </h1>
+        </div>
+        <div className="flex flex-col justify-center items-center gap-4 lg:gap-6 w-full md:w-1/2 lg:w-1/3 h-1/2  ">
+          <p className="text-wrap w-full h-full">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos
+            aliquid rerum beatae, non ea officia repellat architecto? Alias
+            tenetur natus aspernatur earum quibusdam perspiciatis, est error
+            minus eveniet beatae sunt.
+          </p>
+          {/* Buttons */}
+          <div className="flex flex-col sm:flex-row w-full gap-4">
+            <Link
+              href="/projects"
+              className="px-8 py-4 bg-white hover:bg-primary_lighterColor-200 text-black rounded-full border border-black w-full sm:w-[160px] text-center"
+            >
+              Get Started
+            </Link>
+            <Link
+              href="/contact"
+              className="px-8 py-4 bg-primary_backgroundColor-900 hover:bg-primary_backgroundColor-800 text-white rounded-full border border-white w-full  sm:w-[160px] text-center"
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
       </div>
     </main>
   );
