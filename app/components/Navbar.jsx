cbe70258fe81877e5d497f1da8f6da5a1b1c1a0a
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-25 17:51:49 +0200

    Initial navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <div className="max-w-screen-3xl mx-auto w-full h-20">Navbar</div>;
 };
 
 export default Navbar;
