77c9bc641635de3c387335266895d51b8af1795f
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-25 17:30:15 +0200

    Adding additional settings for animation demo

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,6 +40,16 @@ module.exports = {
         },
       },
     },
+    // This is for an animation demo
+    animation: {
+      "infinite-scroll": "infinite-scroll 25s linear infinite",
+    },
+    keyframes: {
+      "infinite-scroll": {
+        from: { transform: "translateX(0)" },
+        to: { transform: "translateX(-50%)" },
+      },
+    },
   },
   plugins: [],
 };
