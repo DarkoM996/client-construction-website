70bc632022877c96bba023333b2fc4e03ce33ef6
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-25 18:01:46 +0200

    Navbar works now

--- app/components/Navlink.jsx
+++ app/components/Navlink.jsx
@@ -1,7 +1,31 @@
+"use client";
 import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-const Navlink = () => {
-  return <div>Navlink</div>;
+const Navlink = ({ link }) => {
+  const pathName = usePathname();
+
+  console.log(pathName);
+  return (
+    <Link
+      href={link.url}
+      className={
+        pathName === link.url ? "bg-white text-black rounded-full p-2" : ""
+      }
+    >
+      {link.title}
+    </Link>
+  );
 };
 
 export default Navlink;
+
+// <Link
+// className={`rounded-xl p-2 ${
+//   pathName === link.url && "bg-white text-black"
+// }`}
+// href={link.url}
+// >
+// {link.title}
+// </Link>
