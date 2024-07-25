64c5215392ebcd4f4759e5bcec185be36f6375e1
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-25 18:18:46 +0200

    Footer will be added individually for other pages

--- app/page.js
+++ app/page.js
@@ -1,8 +1,7 @@
-import Image from "next/image";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import HeroAboutSection from "./components/HeroAboutSection";
 import HoverImageLinks from "./components/HoverImageLinks";
-import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
@@ -10,6 +9,7 @@ export default function Home() {
       <Hero />
       <HoverImageLinks />
       <HeroAboutSection />
+      <Footer />
     </>
   );
 }
