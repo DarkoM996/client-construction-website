5eaa048649d846e411f17c7a189229f81f2f7790
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-25 17:59:03 +0200

    clean up in the homepage

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
 import Image from "next/image";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className="text-4xl font-bold">
-      <div>
-        <h1>Welcome back to another version of this project</h1>
-      </div>
-    </main>
+    <>
+      <Hero />
+    </>
   );
 }
