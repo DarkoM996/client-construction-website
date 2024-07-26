1744daea58ac17b0a448733105d711ed575ae0fa
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-26 15:52:35 +0200

    New section added

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ContactHero from "../components/ContactHero";
+import ContactSection from "../components/ContactSection";
 
 const page = () => {
   return (
     <>
       <ContactHero />
+      <ContactSection />
     </>
   );
 };
