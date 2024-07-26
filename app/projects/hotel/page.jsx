3c822a6dd7defb8acdf196e930fa77c3e4b6ac43
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-26 16:35:25 +0200

    latest

--- app/projects/hotel/page.jsx
+++ app/projects/hotel/page.jsx
@@ -1,7 +1,14 @@
+import HotelAbout from "@/app/components/HotelAbout";
+import HotelHero from "@/app/components/HotelHero";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <HotelHero />
+      <HotelAbout />
+    </div>
+  );
 };
 
 export default page;
