6211a007ba32d13a4d1ed9ba236a73da59568e09
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-27 21:08:10 +0200

    test for production

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -10,6 +10,8 @@ const Hero = () => {
       <div className="relative h-2/3">
         <Image
           priority={true}
+          height={600}
+          width={900}
           src={hero}
           alt="hero image"
           // just experiment with different height of the image at different width sizes. The original h-[600px] can also work.
