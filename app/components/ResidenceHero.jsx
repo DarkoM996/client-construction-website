98ee041190f00dacebd59b79be531e2539e1606d
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-27 04:04:49 +0200

    new addition,might turn this into a separate page

---
+++ app/components/ResidenceHero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ResidenceHero = () => {
+  return (
+    <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4 md:px-12 md:py-10 lg:px-28">
+      <div className="flex flex-col justify-center items-center w-full h-[80%] gap-6">
+        <h1 className="text-5xl md:text-6xl lg:text-8xl">Work In Progress</h1>
+        <button className="px-6 py-4 bg-white text-black rounded-full">
+          Back to projects
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default ResidenceHero;
--- app/projects/residencebuilding/page.jsx
+++ app/projects/residencebuilding/page.jsx
@@ -1,7 +1,12 @@
+import ResidenceHero from "@/app/components/ResidenceHero";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <ResidenceHero />
+    </>
+  );
 };
 
 export default page;
