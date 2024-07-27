12bb84d7684e3d48df98663b3bef285ab5d4b490
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-27 04:10:56 +0200

    Added work in progress

---
+++ app/projects/luxuryhome/page.jsx
@@ -0,0 +1,12 @@
+import WorkInProgress from "@/app/components/WorkInProgress";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <WorkInProgress />
+    </>
+  );
+};
+
+export default page;
