4b1970d7feaf4be54a6527373cd5fa1d1bdade21
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-27 21:11:01 +0200

    testing with smaller image layout on mobile

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <main className="max-w-screen-3xl mx-auto w-full min-h-screen relative px-6 py-4 lg:px-28">
       {/* Image Container */}
-      <div className="relative h-2/3">
+      <div className="relative h-1/2 md:h-2/3">
         <Image
           priority={true}
           height={600}
