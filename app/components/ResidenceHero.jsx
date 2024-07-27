c5ec6363519eafacace6e33ccd7e26da7216b9ca
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-27 04:05:52 +0200

    latest

--- app/components/ResidenceHero.jsx
+++ app/components/ResidenceHero.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const ResidenceHero = () => {
@@ -5,9 +6,12 @@ const ResidenceHero = () => {
     <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4 md:px-12 md:py-10 lg:px-28">
       <div className="flex flex-col justify-center items-center w-full h-[80%] gap-6">
         <h1 className="text-5xl md:text-6xl lg:text-8xl">Work In Progress</h1>
-        <button className="px-6 py-4 bg-white text-black rounded-full">
+        <Link
+          href="/projects"
+          className="px-6 py-4 bg-white text-black rounded-full"
+        >
           Back to projects
-        </button>
+        </Link>
       </div>
     </section>
   );
