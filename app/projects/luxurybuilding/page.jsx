56f67f242c7495863f1fd842bd28d18db457823d
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-27 03:25:38 +0200

    latest

--- app/projects/luxurybuilding/page.jsx
+++ app/projects/luxurybuilding/page.jsx
@@ -1,3 +1,4 @@
+import LuxuryAbout from "@/app/components/LuxuryAbout";
 import LuxuryHero from "@/app/components/LuxuryHero";
 import React from "react";
 
@@ -5,6 +6,7 @@ const page = () => {
   return (
     <>
       <LuxuryHero />
+      <LuxuryAbout />
     </>
   );
 };
