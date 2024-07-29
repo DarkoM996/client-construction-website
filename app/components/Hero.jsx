a98efd5b529ce287de60c6b1d035b9c6ee024783
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-29 13:15:41 +0200

    latest test

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
           src={hero}
           alt="hero image"
           // just experiment with different height of the image at different width sizes. The original h-[600px] can also work.
-          className="w-full h-[320px] sm:h-[600px] object-cover rounded-3xl bg-center aspect-square"
+          className="w-full h-full object-cover rounded-3xl bg-center aspect-square"
         />
       </div>
       {/* Text */}
