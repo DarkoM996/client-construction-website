8eab86f801e4c05d35ea942f5338f363e2ebb5b3
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-08-04 22:12:32 +0200

    latest

---
+++ app/not-found.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NotFound = () => {
+  return <div>not-NotF</div>;
+};
+
+export default NotFound;
+
+// if you are creating a not found page with the short rfce.. you have to rename the initial stuff because it will throw an error for some reason. There's also NetNinja style of writing an error 404 page which works, I will stick to this method which works for me better.
