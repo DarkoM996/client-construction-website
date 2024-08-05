c685f034b741e9825dcbe004ca63ab7dde7b7059
DarkoM996 <87906810+DarkoM996@users.noreply.github.com>
2024-08-05 11:52:30 +0200

    Develop to main (#2)
    * testing a change
    
    * test
    
    * test
    
    * test
    
    * test
    
    * testing latest
    
    * revert back
    
    * another test
    
    * reverting back
    
    * test
    
    * another test
    
    * another test
    
    * revert
    
    * one final test for today
    
    * revert
    
    * test
    
    * correction
    
    * test
    
    * latest
    
    * latest
    
    * one more test
    
    * latest
    
    * test
    
    * latest test
    
    * latest
    
    * latest
    
    * latest
    
    * something for later
    
    * test
    
    * image optimization should be better now
    
    * more  testing for images
    
    * latest
    
    * latest
    
    * test
    
    * test
    
    * test
    
    * revert
    
    * latest
    
    * corrections for responsiveness on mobile
    
    * latest
    
    * Error 404 page added
    
    * Fixing responsiveness issues on mobile
    
    * more layout fixes on mobile
    
    * latest

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import React from "react";
-import hero from "../../public/images/hero.jpg";
+import hero from "../public/images/picture8.jpg";
 import Link from "next/link";
 
-const Hero = () => {
+const NotFound = () => {
   return (
-    <main className="max-w-screen-3xl mx-auto w-full h-fit md:min-h-screen relative px-6 py-4 lg:px-28">
+    <main className="max-w-screen-3xl mx-auto w-full md:min-h-screen relative px-6 py-4 lg:px-28">
       <div className="flex flex-col justify-center items-start">
         {/* Test for change */}
         {/* Image Container */}
@@ -24,29 +24,28 @@ const Hero = () => {
         <div className="h-full w-full flex flex-col justify-center items-center md:flex-row md:justify-between gap-8 py-5">
           <div className="w-full md:w-1/2 lg:w-full">
             <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold text-wrap w-full lg:w-2/3 h-full">
-              Hotel Construction Amplified
+              Error 404. Something went wrong!
             </h1>
           </div>
           <div className="flex flex-col justify-center items-center gap-4 lg:gap-6 w-full md:w-1/2 lg:w-1/3 h-1/2  ">
             <p className="text-wrap w-full h-full">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos
-              aliquid rerum beatae, non ea officia repellat architecto? Alias
-              tenetur natus aspernatur earum quibusdam perspiciatis, est error
-              minus eveniet beatae sunt.
+              Unfortunately something went wrong. The page that you were looking
+              doesn't exist. You can go back to the homepage from here or go to
+              the projects to select other projects.
             </p>
             {/* Buttons */}
             <div className="flex flex-col sm:flex-row w-full gap-4">
               <Link
-                href="/projects"
+                href="/"
                 className="px-8 py-4 bg-white hover:bg-primary_lighterColor-200 text-black rounded-full border border-black w-full sm:w-[160px] text-center"
               >
-                Get Started
+                Homepage
               </Link>
               <Link
-                href="/contact"
+                href="/projects"
                 className="px-8 py-4 bg-primary_backgroundColor-900 hover:bg-primary_backgroundColor-800 text-white rounded-full border border-white w-full  sm:w-[160px] text-center"
               >
-                Contact
+                Projects
               </Link>
             </div>
           </div>
@@ -56,4 +55,6 @@ const Hero = () => {
   );
 };
 
-export default Hero;
+export default NotFound;
+
+// if you are creating a not found page with the short rfce.. you have to rename the initial stuff because it will throw an error for some reason. There's also NetNinja style of writing an error 404 page which works, I will stick to this method which works for me better.
