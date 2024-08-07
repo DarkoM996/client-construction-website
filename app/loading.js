53a7beb82820a85ff8108b7e9526333142efac43
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-08-07 18:33:06 +0200

    testing a bigger font for this part

--- app/loading.js
+++ app/loading.js
@@ -2,7 +2,7 @@ export default function () {
   return (
     <main className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4 lg:px-28">
       <div className="flex flex-col justify-center items-center w-full h-full">
-        Loading ...
+        <h1 className="text-5xl md:text-6xl lg:text-7xl">Loading...</h1>
       </div>
     </main>
   );
