e52601b57ba119b3649c3816b0ecfe875f8000f8
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-08-07 16:18:33 +0200

    test

--- app/not-found.js
+++ app/not-found.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import hero from "../public/images/picture8.jpg";
+import hero from "../public/images/picture10.jpg";
 
 export default function NotFound() {
   return (
